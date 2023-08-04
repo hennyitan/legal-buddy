3809c0cdb728e8fb7245862acc3f3f95a2a8b90f
Mitchellindev <mitchellin.tessy@gmail.com>
2023-08-04 03:13:34 +0100

    updated

---
+++ AI chatbot/script.js
@@ -0,0 +1,60 @@
+const chatbox = document.getElementById("chatbox");
+const userInput = document.getElementById("userInput");
+
+function sendMessage() {
+	const userMessage = userInput.value.trim();
+	if (userMessage === "") {
+		return;
+	}
+
+	addMessage("User: " + userMessage);
+	userInput.value = "";
+
+	setTimeout(() => processUserMessage(userMessage), 1000);
+}
+
+function addMessage(message) {
+	const messageElem = document.createElement("div");
+	messageElem.classList.add("message");
+	messageElem.textContent = message;
+	chatbox.appendChild(messageElem);
+}
+
+function processUserMessage(message) {
+	if (
+		message.toLowerCase().includes("hi") ||
+		message.toLowerCase().includes("hello")
+	) {
+		addMessage(
+			"AI Chatbot: Thank you for introducing yourself. How can I assist you today? Are you looking for a lawyer to draft agreements or handle a legal matter?"
+		);
+	} else if (message.toLowerCase().includes("draft agreement")) {
+		addMessage(
+			"AI Chatbot: Great! I can help with that. To better assist you, could you please provide me with some details about the agreement you need?"
+		);
+	} else if (message.toLowerCase().includes("rental property lease")) {
+		addMessage(
+			"AI Chatbot: Perfect! I'll match you with lawyers in your location who can help with rental property lease agreements. Please hold on for a moment while I find the best matches for you."
+		);
+		// Simulate processing and presenting lawyer list...
+		setTimeout(() => {
+			addMessage(
+				"AI Chatbot: Here are some lawyers in your location who specialize in rental property lease agreements. You can review their profiles and choose the one that suits you best. If you have any questions or need further assistance, feel free to ask."
+			);
+		}, 2000);
+	} else if (message.toLowerCase().includes("abused at work")) {
+		addMessage(
+			"AI Chatbot: I'm really sorry to hear that you're going through this. Your well-being is essential to us. We have lawyers who handle pro bono cases for individuals facing such issues. I'll match you with them so they can provide you with the necessary support. Please give me a moment."
+		);
+		// Simulate processing and presenting pro bono lawyer list...
+		setTimeout(() => {
+			addMessage(
+				"AI Chatbot: Here are some lawyers who specialize in pro bono cases related to workplace abuse. They can assist you further. Take your time to review their profiles, and if you have any questions, feel free to ask."
+			);
+		}, 2000);
+	} else {
+		addMessage(
+			"AI Chatbot: I'm sorry, but I'm not able to assist with that specific issue. Can I help you with anything else?"
+		);
+	}
+}
